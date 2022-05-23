eaa7bd505ccaab8fcf8341f627ace2ab9e2f007e
Anouar-91 <a.berrouane@hotmail.com>
2022-05-23 15:34:57 +0200

    updateProfil

--- src/components/Profil/UpdateProfil.js
+++ src/components/Profil/UpdateProfil.js
@@ -46,8 +46,8 @@ const UpdateProfil = () => {
           <h3>Photo de profil</h3>
           <img src={userData.picture} alt="user-pic" />
           <UploadImg />
-          <p>{error.maxSize}</p>
-          <p>{error.format}</p>
+{/*           <p>{error.maxSize}</p>
+          <p>{error.format}</p> */}
         </div>
         <div className="right-part">
           <div className="bio-update">
